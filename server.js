3ddda9521c6351259fa93406bc21686bfe9e9821
Malek Hamdan <malekhamdan82@gmail.com>
2022-08-06 10:54:24 +0300

    Add the movies and weather Api

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const weatherData = require("./data/weather.json");
+const axios = require("axios");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -13,25 +14,44 @@ app.get("/", (req, res) => {
   res.send("Test Server!");
 });
 
-app.get("/weather", (req, res) => {
+app.get("/weather", handleWeather);
+
+app.get("/movies", handleMovies);
+
+async function handleWeather(req, res) {
   const lat = req.query.lat;
   const lon = req.query.lon;
   const searchQuery = req.query.searchQuery;
 
-  const cityDataArr = weatherData.find(
-    (item) =>
-      item.city_name.toLocaleLowerCase() === searchQuery.toLocaleLowerCase()
-  );
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
+  const weatherApi = await axios.get(url);
 
   try {
-    const cityData = cityDataArr.data.map((item) => new Forecast(item));
+    const weatherDay = weatherApi.data.data.map((item) => {
+      return new Forecast(item);
+    });
 
-    res.status(200).send(cityData);
+    res.status(200).send(weatherDay);
   } catch (error) {
-    errorHandler(error, res);
+    errorHandler(error);
   }
-});
+}
 
+async function handleMovies(req, res) {
+  const searchQuery = req.query.searchQuery;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
+  const moviesArr = await axios.get(url);
+
+  try {
+    const movieInfo = moviesArr.data.results.map((item) => {
+      return new Movies(item);
+    });
+    console.log(movieInfo);
+    res.status(200).send(movieInfo);
+  } catch (error) {
+    errorHandler(error);
+  }
+}
 app.get("*", (req, res) => {
   res.status(404).send("Page Not Found!!");
 });
@@ -43,6 +63,19 @@ class Forecast {
   constructor(day) {
     this.date = day.valid_date;
     this.descrption = day.weather.description;
+    this.cityName = day.timezone;
+  }
+}
+
+class Movies {
+  constructor(movieInfo) {
+    this.title = movieInfo.title;
+    this.overview = movieInfo.overview;
+    this.average_votes = movieInfo.vote_average;
+    this.total_votes = movieInfo.vote_count;
+    this.image_url = movieInfo.poster_path;
+    this.popularity = movieInfo.popularity;
+    this.released_on = movieInfo.release_date;
   }
 }
 app.listen(PORT, () => {
